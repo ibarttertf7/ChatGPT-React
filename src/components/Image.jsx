6ce614b60c20f258f128b9b9b05275eadbb6be8f
ibarttertf7 <85906816+ibarttertf7@users.noreply.github.com>
2023-06-08 10:46:56 -0700

    init

---
+++ src/components/Image.jsx
@@ -0,0 +1,64 @@
+
+
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import "./Image.css";
+
+function Image() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [placeholder, setPlaceholder] = useState(
+    "Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh.."
+  );
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_Open_AI_Key,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    setPlaceholder(`Search ${prompt}..`);
+    setLoading(true);
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
+    });
+    setLoading(false);
+    setResult(res.data.data[0].url);
+  };
+  return (
+    <div className="app-main">
+      {loading ? (
+        <>
+          <h2>Generating image...</h2>
+          <div class="lds-ripple">
+            <div></div>
+            <div></div>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>Generate an Image using DALL·E</h2>
+
+          <textarea
+            className="app-input"
+            placeholder={placeholder}
+            onChange={(e) => setPrompt(e.target.value)}
+            rows="10"
+            cols="40"
+          />
+          <button className="btn" onClick={generateImage}>Generate Image</button>
+          {result.length > 0 ? (
+            <img className="result-image" src={result} alt="result" />
+          ) : (
+            <></>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Image;
